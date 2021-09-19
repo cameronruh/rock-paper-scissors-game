4d7b0eeec84eaf9305064a6d18b6d7f5dea01adb
cameronruh <89526567+cameronruh@users.noreply.github.com>
2021-09-19 13:40:41 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,9 @@
 # this is the "game.py" file...
 
 print("Rock, Paper, Scissors, Shoot!")
+
+# PROMPT USER FOR INPUT
+
+#x = input("Choose 'rock' or 'paper' or 'scissors'")
+user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+print(user_choice)
