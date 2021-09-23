0952ee9e444326df87b4391bdf0d25bfe0719358
cameronruh <89526567+cameronruh@users.noreply.github.com>
2021-09-23 18:30:14 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,12 +2,20 @@
 
 import random
 
-print("Rock, Paper, Scissors, Shoot!")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+x = os.getenv("PLAYER_NAME")
+
+print("Welcome", x, "ROCK, PAPER, SCISSORS, SHOOT!")
 
 # PROMPT USER FOR INPUT
 
 #x = input("Choose 'rock' or 'paper' or 'scissors'")
 user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+print("User chose:")
 print(user_choice)
 
 # COMPUTER CHOICE (AT RANDOM)
@@ -15,4 +23,30 @@ print(user_choice)
 options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
+print("Computer chose:")
 print (computer_choice)
+
+if (user_choice == "rock") and (computer_choice == "rock"):
+    print("TIE.")
+elif (user_choice == "scissors") and (computer_choice == "scissors"):
+    print("TIE.")
+elif (user_choice == "paper") and (computer_choice == "paper"):
+    print("TIE.")
+elif (user_choice == "rock") and (computer_choice == "paper"):
+    print("SORRY, YOU LOST. :(")
+elif (user_choice == "rock") and (computer_choice == "scissors"):
+    print("CONGRATULATIONS, YOU WON! :)")
+elif (user_choice == "paper") and (computer_choice == "scissors"):
+    print("SORRY, YOU LOST :(")
+elif (user_choice == "paper") and (computer_choice == "rock"):
+    print("CONGRATULATIONS, YOU WON! :)")
+elif (user_choice == "scissors") and (computer_choice == "rock"):
+    print("SORRY, YOU LOST. :(")
+elif (user_choice == "scissors") and (computer_choice == "paper"):
+    print("CONGRATULATIONS, YOU WON! :)")
+
+#adapted from Ryan Knapick on Slack (https://app.slack.com/client/TERLN9HL6/C02BNKRKZV5)
+elif(user_choice != 'rock' or 'paper' or 'scissors'):
+    print ("Something doesn't look right. Try again, and remember to use lowercase letters only.")
+
+print("THANKS FOR PLAYING. PLEASE PLAY AGAIN!")
